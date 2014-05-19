ee5192346930d48ed95717f8e725f6654888bacf
Arman Bilge <armanbilge@gmail.com>
2014-05-19 12:12:20 +1200

    Partial implementation of select Taylor degree function

--- src/amh11/AMH11.java
+++ src/amh11/AMH11.java
@@ -25,24 +25,76 @@ package amh11;
 import java.util.Arrays;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
 public class AMH11 {
 
-    private static final void normAm(RealMatrix A, double m) {
+    
+    
+    private static final Object[] selectTaylorDegree(RealMatrix A, RealMatrix b, 
+            int mMax, int pMax, double prec, boolean shift, boolean bal,
+            boolean forceEstm) {
+        int n = A.getRowDimension();
+        if (bal) {
+            throw new RuntimeException("Not implemented!");
+        }
+        double mu;
+        if (shift) {
+            mu = A.getTrace() / n;
+            A = A.add(MatrixUtils.createRealIdentityMatrix(n)
+                    .scalarMultiply(-mu));
+        }
+        double mv = 0.0;
+        double normA = 0.0;
+        if (!forceEstm)
+            normA = A.getNorm();
+        int unA;
+        double[] alpha;
+        if (!forceEstm && normA < 4 * ThetaTaylor.THETA[mMax] * pMax
+                * (pMax + 3) / (mMax * b.getColumnDimension())) {
+            unA = 1;
+            double c = normA;
+            alpha = new double[pMax - 1];
+            Arrays.fill(alpha, 1.0);
+        } else {
+            unA = 0;
+            double[] eta = new double[pMax - 1];
+            alpha = new double[pMax - 1];
+            for (int p = 0; p < pMax; ++p) {
+                double[] ck = normAm(A, p+1);
+                double c = Math.pow(ck[0], 1/(p+1));
+                mv = mv + ck[1];
+                eta[p] = c;
+            }
+            for (int p = 0; p < pMax - 1; ++p) {
+                alpha[p] = Math.max(eta[p], eta[p+1]);
+            }
+        }
+        RealMatrix M = new Array2DRowRealMatrix(mMax, pMax-1);
+        for (int p = 1; p < pMax; ++p) {
+            for (int m = p * (p-1) - 1; m < mMax; ++m)
+                M.setEntry(m, p-1, alpha[p-1] / ThetaTaylor.THETA[m]);
+        }
+        return new Object[]{M, mv, alpha, unA};
+    }
+    
+    private static final double[] normAm(RealMatrix A, int m) {
         int t = 1;
-        int n = A.getColumnDimension();
+        int n = A.getRowDimension();
+        double c, mv;
         if (A.equals(abs(A))) {
             double[] ones = new double[n];
             Arrays.fill(ones, 1.0);
             RealMatrix e = new Array2DRowRealMatrix(ones);
             for (int j = 0; j < m; ++j)
                 e = A.transpose().multiply(e);
-            double c = e.getRowVector(0).getLInfNorm();
-            double mv = m;
+            c = e.getRowVector(0).getLInfNorm();
+            mv = m;
         } else {
             throw new RuntimeException("Not implemented!");
         }
+        return new double[]{c, mv};
     }
         
     private static final RealMatrix abs(RealMatrix A) {
